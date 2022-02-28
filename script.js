3cc0245d2213299100d1de7305545b83bd828241
kidosol5847 <59013959+kidosol5847@users.noreply.github.com>
2022-02-28 14:16:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const quotes =[
+    {name:'a',
+    quote:'a'},
+    {name:'b',
+    quote:'b'},
+    {name:'c',
+    quote:'c'},
+    {name:'d',
+    quote:'d'},
+    {name:'e',
+    quote:'e'},
+]
+
+const a = document.querySelector('#quoteBtn');
+const b = document.querySelector('#quoteAuthor');
+const c = document.querySelector('#quote');
+
+a.addEventListener(click,d)
+function d(){
+    let num = Math.floor(Math.random()*quotes.length);
+    b.innerHTML = quotes[num].name;
+    c.innerHTML = quotes[num].quote;
+}
